c5ce4b2a638850399c2341ba5dfe572c7e970b77
Dmitry Dygalo <d.dygalo@volsor.com>
2014-06-09 17:09:08 +0200

    add tests

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     version=__version__,
     description='Small handler and formatter for using python logging with Redis',
     url='https://github.com/lobziik/rlog',
-    tests_require=['pytest>=2.5.0'],
+    tests_require=['pytest>=2.5.0', 'mock'],
     install_requires=['redis', 'ujson'],
     packages=['rlog'],
     cmdclass={'test': PyTest},
