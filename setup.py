3308a8c2d504ef98d7c61ca6554a742106e8f32a
lobziik <lobziiko.o@gmail.com>
2014-06-10 02:50:15 +0800

    try to add coverage report

--- setup.py
+++ setup.py
@@ -2,28 +2,6 @@
 # coding: utf-8
 import sys
 
-try:
-    from setuptools import setup
-    from setuptools.command.test import test as TestCommand
-
-    class PyTest(TestCommand):
-
-        def finalize_options(self):
-            TestCommand.finalize_options(self)
-            self.test_args = []
-            self.test_suite = True
-
-        def run_tests(self):
-            # import here, because outside the eggs aren't loaded
-            import pytest
-            errno = pytest.main(self.test_args)
-            sys.exit(errno)
-
-except ImportError:
-
-    from distutils.core import setup
-    PyTest = lambda x: x
-
 setup(
     name='rlog',
     version='0.0.1',
@@ -31,6 +9,5 @@ setup(
     url='https://github.com/lobziik/rlog',
     tests_require=['pytest>=2.5.0', 'mock'],
     install_requires=['redis', 'ujson'],
-    packages=['rlog'],
-    cmdclass={'test': PyTest}
+    packages=['rlog', 'tests']
 )
