ab891fe86df963f7ea79dea796067d60effd7cf7
lobziik <lobziiko.o@gmail.com>
2014-06-10 02:53:29 +0800

    add missed import

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 import sys
+from setuptools import setup
+
 
 setup(
     name='rlog',
@@ -9,5 +11,5 @@ setup(
     url='https://github.com/lobziik/rlog',
     tests_require=['pytest>=2.5.0', 'mock'],
     install_requires=['redis', 'ujson'],
-    packages=['rlog', 'tests']
+    packages=['rlog', 'tests'],
 )
