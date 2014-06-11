6b32c5d406524c8a90163b1d6b490cb52dd08baf
lobziik <lobziiko.o@gmail.com>
2014-06-12 01:46:38 +0800

    udp setup.py, bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='rlog',
-    version='0.0.1',
+    version='0.0.2',
     description='Small handler and formatter for using python logging with Redis',
     url='https://github.com/lobziik/rlog',
     tests_require=['pytest>=2.5.0', 'mock'],
@@ -25,8 +25,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6'
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2'
+        'Programming Language :: Python :: 3.3'
         'Programming Language :: Python :: 3.4'
     ]
 )
